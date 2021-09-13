5d63f428cbbf465b9a7b56156d34713054df9bce
pawan.verma <pawan.verma@appdirect.com>
2021-09-13 12:14:59 +0530

    some more array functions

--- src/Student.java
+++ src/Student.java
@@ -1,3 +1,6 @@
+import java.math.BigDecimal;
+import java.util.Arrays;
+
 public class Student {
 
     String name;
@@ -8,8 +11,45 @@ public class Student {
         this.name = name;
     }
 
-    public static void main(String[] args) {
+    int numberOfMarks() {
+        return marks.length;
+    }
 
+    int sumOfMarks() {
+        int sum = 0;
+
+        for (int mark : marks) {
+            sum = sum + mark;
+        }
+        return sum;
 
     }
+
+    int maxMarks() {
+        int max = 0;
+        for (int mark : marks) {
+            if (mark > max) {
+                max = mark;
+            }
+        }
+        return max;
+    }
+
+    int minMarks() {
+
+        int min = 100;
+        for (int mark : marks) {
+            if (mark < min) {
+                min = mark;
+            }
+        }
+        return min;
+
+    }
+
+    double averageMarks() {
+        int s = sumOfMarks();
+        return s / marks.length;
+    }
 }
+
--- src/StudentMarks.java
+++ src/StudentMarks.java
@@ -1,8 +1,13 @@
 public class StudentMarks {
 
     public static void main(String[] args) {
-        int [] marks={80,87,86,98};
+        int [] marks={80,60,71,99};
         Student stu= new Student("pawan",marks);
+        System.out.println(stu.numberOfMarks());
+        System.out.println(stu.sumOfMarks());
+        System.out.println(stu.maxMarks());
+        System.out.println(stu.minMarks());
+        System.out.println(stu.averageMarks());
 
     }
 }
